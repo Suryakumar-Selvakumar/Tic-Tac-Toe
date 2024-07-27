a33fcdb2a9b196c185255aa2418d1ae3e4f0fe5f
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-07-27 14:26:49 -0600

    Finish the working Console Version of the game

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let player = "p1";
-
 const gameBoard = (function () {
   let board = [
     ["", "", ""],
@@ -12,15 +10,34 @@ const gameBoard = (function () {
   }
 
   function insertMarker(player, row, column) {
-    if (player === "p1") {
-      board[row][column] = "X";
-    } else if (player === "p2") {
-      board[row][column] = "O";
+    if (board[row][column] === "X" || board[row][column] === "O") {
+      let rowNew, columnNew;
+      do {
+        alert("Position occupied by marker, pick another position!");
+        do {
+          rowNew = parseInt(prompt("Enter Marker row: "), 10);
+        } while (!(rowNew >= 0 && rowNew < 3) && !rowNew === null);
+        do {
+          columnNew = parseInt(prompt("Enter Marker column: "), 10);
+        } while (!(columnNew >= 0 && columnNew < 3) && !columnNew === null);
+      } while (rowNew === row && columnNew === column);
+      if (player === "p1") {
+        board[rowNew][columnNew] = "X";
+      } else if (player === "p2") {
+        board[rowNew][columnNew] = "O";
+      }
+    } else {
+      if (player === "p1") {
+        board[row][column] = "X";
+      } else if (player === "p2") {
+        board[row][column] = "O";
+      }
     }
   }
 
   function switchTurn(player) {
     player = player === "p1" ? "p2" : "p1";
+    return player;
   }
 
   function winCheck(marker, row, column) {
@@ -76,48 +93,53 @@ function playerCreator(marker) {
   }
 
   function getMarkerPosition() {
-    const row = parseInt(prompt("Enter Marker row: ", ""));
-    const column = parseInt(prompt("Enter Marker column: ", ""));
+    let row, column;
+    do {
+      row = parseInt(prompt("Enter Marker row: "), 10);
+    } while (!(row >= 0 && row < 3) && !row === null);
+    do {
+      column = parseInt(prompt("Enter Marker column: "), 10);
+    } while (!(column >= 0 && column < 3) && !column === null);
     return [row, column];
   }
 
   return { setMarker, getMarkerPosition };
 }
 
-const player1 = playerCreator("X");
-const player2 = playerCreator("O");
-
-(function playGame(outerPlayer) {
-  console.log("Player 1: X && Player 2: O");
+(function playGame() {
+  let player = "p1";
+  const player1 = playerCreator("X");
+  const player2 = playerCreator("O");
+  console.log("Player 1: X | Player 2: O");
   let winStatus = false;
   let tieStatus = false;
   let currentMarker = "";
-  const row = 0,
+  let row = 0,
     column = 0;
   do {
-    if (outerPlayer === "p1") {
-      console.log("Enter player 1's marker position in the prompt");
+    if (player === "p1") {
+      console.log("Enter player 1's marker (X) position in the prompt");
       [row, column] = player1.getMarkerPosition();
       currentMarker = player1.setMarker();
-    } else if (outerPlayer === "p2") {
-      console.log("Enter player 2's marker position in the prompt");
+    } else if (player === "p2") {
+      console.log("Enter player 2's marker (O) position in the prompt");
       [row, column] = player2.getMarkerPosition();
       currentMarker = player2.setMarker();
     }
-    gameBoard.insertMarker(outerPlayer, row, column);
+    gameBoard.insertMarker(player, row, column);
     gameBoard.viewBoard();
     winStatus = gameBoard.winCheck(currentMarker, row, column);
     tieStatus = gameBoard.tieCheck(winStatus);
-    gameBoard.switchTurn(outerPlayer);
+    player = gameBoard.switchTurn(player);
   } while (!winStatus && !tieStatus);
 
   if (tieStatus === true) {
-    return "It's a Tie!";
+    console.log("It's a Tie!");
   } else {
     if (currentMarker === "X") {
-      return "Player-1(X) has won the game!";
+      console.log("Player-1(X) has won the game!");
     } else if (currentMarker === "O") {
-      return "Player-2(O) has won the game!";
+      console.log("Player-2(O) has won the game!");
     }
   }
 })();
